1659f5156dfeaa492c928b99b1a6cc24c1e7f902
oscarheimdahl <o.heimdahl@gmail.com>
2022-12-02 13:50:52 +0100

    Day 2

--- day02/index.test.ts
+++ day02/index.test.ts
@@ -3,40 +3,24 @@ import { getSolutionPart1, getSolutionPart2 } from './index.ts';
 
 // Part 1
 
-Deno.test('Should get one big over many small', () => {
-  const lines = ['1', '1', '1', '1', '1', '1', '1', '', '10'];
-  const max = getSolutionPart1(lines);
-  assertEquals(max, 10);
-});
-
-Deno.test('Should get biggest in increasing sequence', () => {
-  const lines = ['1', '', '2', '', '3', '', '4'];
-  const max = getSolutionPart1(lines);
-  assertEquals(max, 4);
-});
-
-Deno.test('Should get biggest in decreasing sequence', () => {
-  const lines = ['4', '', '3', '', '2', '', '1'];
-  const max = getSolutionPart1(lines);
-  assertEquals(max, 4);
-});
-
 // Part 2
 
-Deno.test('Should get one big over many small', () => {
-  const lines = ['1', '1', '1', '1', '1', '1', '1', '', '10'];
-  const max = getSolutionPart2(lines);
-  assertEquals(max, 17);
+const win = 6; // Z
+const draw = 3; // Y
+const loss = 0; // X
+
+const rock = 1; // A
+const paper = 2; // B
+const scissors = 3; // C
+
+Deno.test('[Rock | Win] should win with paper', () => {
+  const lines = ['A Z'];
+  const score = getSolutionPart2(lines);
+  assertEquals(score, win + paper);
 });
 
-Deno.test('Should get biggest in increasing sequence', () => {
-  const lines = ['1', '', '2', '', '3', '', '4'];
-  const max = getSolutionPart2(lines);
-  assertEquals(max, 9);
-});
-
-Deno.test('Should get biggest in decreasing sequence', () => {
-  const lines = ['4', '', '3', '', '2', '', '1'];
-  const max = getSolutionPart2(lines);
-  assertEquals(max, 9);
+Deno.test('[Rock | Draw] should draw with rock', () => {
+  const lines = ['A Y'];
+  const score = getSolutionPart2(lines);
+  assertEquals(score, draw + rock);
 });
--- day02/index.ts
+++ day02/index.ts
@@ -7,22 +7,112 @@ const movesA = {
 };
 
 const movesB = {
-  rock: 'X',
-  paper: 'Y',
-  scissors: 'Z',
+  rock: { label: 'X', score: 1 },
+  paper: { label: 'Y', score: 2 },
+  scissors: { label: 'Z', score: 3 },
 };
 
-const moveToScore = {
-  X: 1,
-  Y: 2,
-  Z: 3,
+const resultToScore = {
+  loss: 0,
+  draw: 3,
+  win: 6,
 };
 
 export function getSolutionPart1(lines: string[]) {
-  console.log(lines);
+  let sum = 0;
+
+  lines.forEach((line) => {
+    const [a, b] = line.split(' ');
+
+    switch (a) {
+      case movesA.rock: {
+        switch (b) {
+          case movesB.rock.label:
+            return (sum += resultToScore.draw + movesB.rock.score);
+          case movesB.paper.label:
+            return (sum += resultToScore.win + movesB.paper.score);
+          case movesB.scissors.label:
+            return (sum += resultToScore.loss + movesB.scissors.score);
+        }
+        break;
+      }
+      case movesA.paper: {
+        switch (b) {
+          case movesB.rock.label:
+            return (sum += resultToScore.loss + movesB.rock.score);
+          case movesB.paper.label:
+            return (sum += resultToScore.draw + movesB.paper.score);
+          case movesB.scissors.label:
+            return (sum += resultToScore.win + movesB.scissors.score);
+        }
+        break;
+      }
+      case movesA.scissors: {
+        switch (b) {
+          case movesB.rock.label:
+            return (sum += resultToScore.win + movesB.rock.score);
+          case movesB.paper.label:
+            return (sum += resultToScore.loss + movesB.paper.score);
+          case movesB.scissors.label:
+            return (sum += resultToScore.draw + movesB.scissors.score);
+        }
+        break;
+      }
+    }
+  });
+  return sum;
 }
 
-export function getSolutionPart2(lines: string[]) {}
+export function getSolutionPart2(lines: string[]) {
+  const res = {
+    win: 'Z',
+    draw: 'Y',
+    loss: 'X',
+  };
+  let sum = 0;
+
+  lines.forEach((line) => {
+    console.log({ line });
+    const [a, b] = line.split(' ');
+
+    switch (a) {
+      case movesA.rock: {
+        switch (b) {
+          case res.win:
+            return (sum += resultToScore.win + movesB.paper.score);
+          case res.draw:
+            return (sum += resultToScore.draw + movesB.rock.score);
+          case res.loss:
+            return (sum += resultToScore.loss + movesB.scissors.score);
+        }
+        break;
+      }
+      case movesA.paper: {
+        switch (b) {
+          case res.win:
+            return (sum += resultToScore.win + movesB.scissors.score);
+          case res.draw:
+            return (sum += resultToScore.draw + movesB.paper.score);
+          case res.loss:
+            return (sum += resultToScore.loss + movesB.rock.score);
+        }
+        break;
+      }
+      case movesA.scissors: {
+        switch (b) {
+          case res.win:
+            return (sum += resultToScore.win + movesB.rock.score);
+          case res.draw:
+            return (sum += resultToScore.draw + movesB.scissors.score);
+          case res.loss:
+            return (sum += resultToScore.loss + movesB.paper.score);
+        }
+        break;
+      }
+    }
+  });
+  return sum;
+}
 
 const part = Deno.env.get('part') || 'part1';
 
